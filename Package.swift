d89096d52419690c27c2062d759618823edb4751
Piotrek Jeremicz <piotrek@jeremicz.com>
2022-01-13 22:25:10 +0100

    Add resource to the process

--- Package.swift
+++ Package.swift
@@ -5,17 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyMySql",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftyMySql",
             targets: ["SwiftyMySql"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyMySql",
-            dependencies: []),
+            resources: [
+                .process("Query.php")
+            ]),
     ]
 )
